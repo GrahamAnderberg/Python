a78ea14af7847c5d89fa9d0a776dd446fd273d49
GrahamAnderberg <829546@mystma.org>
2021-03-19 15:29:03 +0000

    Mad lib

---
+++ Main.py
@@ -0,0 +1,3 @@
+adjective1 = "Stinky"
+noun1 = "Person"
+print(" There are many " + adjective1 + " ways to choose a " + noun1 + " to read" + " first, you could ask for recommendations from your friends")
