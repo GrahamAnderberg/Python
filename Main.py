fcd33ee94bf4d48ba66bbf9aa18a77d882e62410
Anderberg Graham <829546@stma.k12.mn.us>
2021-03-25 10:25:58 -0500

    Wordsmash

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
-adjective1 = "Stinky"
-noun1 = "Person"
-print(" There are many " + adjective1 + " ways to choose a " + noun1 + " to read" + " first, you could ask for recommendations from your friends")
+def wordSmash(a, b):
+    return a + b
+
+print (wordSmash("Cat", "Dog"))
